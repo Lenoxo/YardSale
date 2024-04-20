fef3ab7fd7e4e1ed457c03d0b4c06e40ce9861c1
Emanuel Padilla <emanuehl159@gmail.com>
2024-04-20 15:09:28 -0500

    Añado pagina de signup y logica basica de login

--- src/login/login.js
+++ src/login/login.js
@@ -1,18 +1,34 @@
 const form = document.querySelector('.form')
+const errorSpan = document.querySelector('#login-error-message')
 
 form.addEventListener('submit', handleLogin)
 
-function handleLogin(event) {
+async function handleLogin(event) {
   event.preventDefault()
   console.log('detected submit')
   const emailVal = event.target.elements.email.value
   const passwordVal = event.target.elements.password.value
 
+  const { savedEmail, savedPassword } = await getUserData()
   const isEmailCorrect = emailVal === savedEmail
   const isPasswordCorrect = passwordVal === savedPassword
-  if (!isEmailCorrect || !isPasswordCorrect) {
-    // Return error to client
-  } else {
-    window.location.href('/')
+
+  try {
+    if (!isEmailCorrect) throw new Error('Check your email again')
+    if (!isPasswordCorrect) throw new Error('Check your password again')
+
+    return window.location.href = '/src'
+  } catch (error) {
+    renderErrorMessage(error.message)
   }
+
+}
+
+function getUserData() {
+  return JSON.parse(localStorage.getItem('user-data'))
+}
+
+function renderErrorMessage(message) {
+  errorSpan.classList.remove('inactive')
+  errorSpan.innerText = message
 }
---
+++ src/signup/signup.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', handleUpdate)
+
+function handleUpdate(event) {
+  event.preventDefault()
+  console.log('detected submit')
+  const nameVal = event.target.elements.name.value
+  const emailVal = event.target.elements.email.value
+  const passwordVal = event.target.elements.password.value
+
+  // TODO: How to validate data from forms using just js, not react like aproaches.
+
+  localStorage.setItem('user-data', JSON.stringify({
+    savedName: nameVal,
+    savedEmail: emailVal,
+    savedPassword: passwordVal
+  }))
+
+  return window.location.href = '/src'
+}
