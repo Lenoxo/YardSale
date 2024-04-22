d617815e42ed124bd315fed91b80836bda0e4efa
Emanuel Padilla <emanuehl159@gmail.com>
2024-04-21 20:36:59 -0500

    Añado valores por defecto en formulario de signup.html
    Para facilitar la actualizacion de datos

--- signup/signup.js
+++ signup/signup.js
@@ -1,4 +1,22 @@
+import { getUserData } from "../utils/getUserData.js";
+
 const form = document.querySelector(".form");
+
+// Fill user info if exists in localStorage for easy update
+
+const userData = getUserData();
+if (userData) {
+  const nameInput = document.querySelector("#name");
+  const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
+
+  const { savedName, savedEmail, savedPassword } = userData;
+  nameInput.defaultValue = savedName;
+  emailInput.defaultValue = savedEmail;
+  passwordInput.defaultValue = savedPassword;
+}
+
+// handle submit event for create / update user data
 
 form.addEventListener("submit", handleUpdate);
 
